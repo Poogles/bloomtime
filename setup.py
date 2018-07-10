16f9efc3031d1b2c15baffce2c2553394f67d146
Sam Pegler <jobs@sampegler.co.uk>
2018-07-10 16:32:33 +0100

    Bug: Fix requires.
    I should really fix the setup.py to be reflective of the requirements

--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ setup(name='bloomtime',
       author='Sam Pegler',
       author_email='sam@sampegler.co.uk',
       license='MIT',
-      install_requires=["pyhash>=0.8.2"],
+      install_requires=["fnvhash>=0.1.0"],
       packages=['bloomtime'],
       zip_safe=False)
